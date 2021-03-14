9dda961df9fb6689e7b90b6da245ac0028eb7920
Leonardo Espinoza <leoespinozidk@gmail.com>
2021-03-14 12:31:55 -0600

    Add project

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,8 @@
 const copyright = document.querySelector("#copyright");
+const projectNumber = document.querySelector("#projectNumber");
+const allProjects = document.querySelectorAll(".project").length;
 
 const currentYear = new Date().getFullYear();
 
+projectNumber.textContent = 16 - parseInt(allProjects);
 copyright.textContent = `© LeoCode0 2021-${currentYear}`;
